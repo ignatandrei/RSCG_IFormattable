46328df586de76947ddc74ffcb4a9aa591257152
Andrei Ignat <ignatandrei@yahoo.com>
2024-07-11 20:14:57 +0300

    v2024.711.2013

--- src/RSCG_IFormattableConsole/Program.cs
+++ src/RSCG_IFormattableConsole/Program.cs
@@ -4,4 +4,4 @@ Person person = new ();
 person.FirstName = "Andrei";
 person.LastName = "Ignat";
 
-Console.WriteLine(person.ToString("The person name is {FirstName} {LastName}",null));
+Console.WriteLine(person.ToString("The person name is {FirstName} {LastName}"));
